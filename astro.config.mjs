c386b8d3b8bf48ffe195b8c9baccc013271c88d1
Edwin Martinez <edwinmartinez@Edwins-MacBook-Pro.local>
2025-05-13 13:16:18 -0500

    commit working_version 05-13

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import './src/styles/global.css';
-
-
 
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  vite: {
-    plugins: [tailwind()],
-  },
 });
